e4e5e8d2a6279d04e8340701160abe6200f1bd81
redandblack-1 <55785921+redandblack-1@users.noreply.github.com>
2020-12-23 14:40:34 +0600

    Add files via upload

---
+++ Problem Set 11/Painter App Modification/src/sample/Main.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene=new Scene(root);
+        stage.setTitle("Painter App Modification");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+}
